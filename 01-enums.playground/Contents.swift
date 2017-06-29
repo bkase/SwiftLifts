30e0b80a09f9e22875f093b84503a81278716f48
Brandon Kase <brandon.kase@gmail.com>
2017-06-28 22:52:34 -0700

    Initial commit; enums

---
+++ 01-enums.playground/Contents.swift
@@ -0,0 +1,233 @@
+/*:
+ # Swift Lift: Eliminating the impossible
+ ## Better Domain Modelling with Enums
+ 
+ What is a Swift Lift?
+ 
+ A short workshop illustrating why some aspect of Swift unlocks some *productivity* gain and why.
+ 
+ What is domain modelling?
+ 
+ The act of expressing your real world task or idea into software. We do this all the time (even if we don't realize it).
+ 
+ In Swift we have the following mantra:
+ 
+ > Insert image on invalid states unrepresentable
+ 
+ Let's start with an example:
+ */
+
+/*:
+ Real Life Problem:
+ 
+ > I'm computing the location of an image on some server. I either have a url string to an image or I have an S3 key and bucket. I always have one or the other and never both.
+ 
+```
+   // equivalent to this representation in ObjC
+   @interface PIImageLocator
+   @property (nonatomic, strong) nullable NSString *url
+   @property (nonatomic, strong) nullable NSString *s3key
+   @property (nonatomic, strong) nullable NSString *s3bucket
+   @end
+```
+ */
+// A "Bad" representation in Swift
+struct PIImageLocator1 {
+    let url: String?
+    let s3key: String?
+    let s3bucket: String?
+    
+    // computed property
+    var realUrl: String {
+        if let url = url {
+            return url
+        } else {
+            return "http://pinterest.s3." +
+                s3bucket! +
+                "." +
+                s3key!
+        }
+    }
+}
+
+/*:
+  Why is the above code a bad model of this problem:
+ 
+ > I'm computing the location of an image on some server. I either have a url string to an image or I have an S3 key and bucket. I always have one or the other and never both.
+*/
+
+/*:
+ Excersize: Why
+ 
+ The following invalid states ARE representable:
+ 1. A key but not a bucket or vice versa
+ 2. Neither the key/bucket nor the url, when the whole thing itself isn't nil (nil,nil,nil)
+ 3. Both a key/bucket and a url
+ ...
+ */
+
+// slightly better
+struct PIImageLocator2 {
+    struct S3Info {
+        let bucket: String
+        let key: String
+    }
+    let url: String?
+    let s3: S3Info?
+    
+    var realUrl: String {
+        if let url = url {
+            return url
+        } else {
+            let info = s3!
+            return "http://pinterest.s3." +
+                info.bucket +
+                "." +
+                info.key
+        }
+    }
+}
+
+//: This is better because we are forced now to pass *both* a key and a bucket if we're passing some s3 information. But it still doesn't stop us from passing nil to both or worse, values to both by accident.
+
+// The invariant is that it's one or the other, so we can whip out an enum for that.
+enum PIImageLocator3 {
+    case url(String)
+    case s3(bucket: String, key: String)
+    
+    var realUrl: String {
+        switch self {
+        case let .url(u): return u
+        case let .s3(bucket: bucket, key: key):
+            return "http://pinterest.s3." +
+                bucket +
+                "." +
+                key
+        }
+    }
+}
+
+/*:
+ Look carefully at the difference between the 3 implementations, what is different?
+ 
+ We don't need to force-unwrap anywhere and we don't need excessive conditionals. This is a sign you did it right.
+
+ Proper domain modelling is translating problem statement to types.
+ 
+ Productivity wins:
+ 
+ 1. Faster dev feedback loop:
+ 
+ You don't need to wait for something to build to find simple mistakes
+ 
+ 2. Faster debug feedback loop:
+ 
+ An entire class of "stupid mistakes" no longer happens
+ 
+ 3. Easier to maintain (readable):
+ 
+ You don't have to read the body of functions or comments to understand constraints. Only types and signatures.
+ Code is FAR more precise than English as long as we all get comfortable with the language.
+ 
+ 4. Easier to maintain (writable):
+ 
+ You don't need to update documentation or assertions or even UNIT TESTS (which as an ios group we'll get better at soon) for the cases you capture in types. There is NO risk of code becoming out of sync with documentation.
+ 
+ */
+
+/*:
+ If we get really good at this we can start to *rely* on types to describe behavior: If I see `String?, String?, String?` that means I'm free to pass nil to any combination of them. You shouldn't crash and even worse, you shouldn't do something that's strange. What does `PIImageLocator1(url: nil, s3key: nil, s3bucket: nil)` even mean?
+ */
+
+//: Excersize
+
+struct User {
+    let name: String
+    // ...
+}
+enum UserState {
+    case loggedOut
+    case loggedIn(User)
+    
+    //: Excersize: Return the name of the user or "Logged out" if no user is logged in.
+    var displayName: String {
+        switch self {
+        case .loggedOut: return "Logged out"
+        case let .loggedIn(user): return user.name
+        }
+    }
+}
+
+
+//: Excersize: We want a data structure that captures the idea of getting a String from the network with a success status code (like 200 "Success") or a failure from our backend (like 503 "Unauthorized") or a failure because airplane mode is on (with no associated data)
+//: Excersize: Now implement a method that logs the successful value or of the failure using `print`
+enum NetworkResult {
+    case success(code: Int, data: String)
+    case backendFailure(code: Int)
+    case airplaneModeFailure
+    
+    var debugString: String {
+        switch self {
+        case let .success(code: code, data: data):
+            return "Success \(code), \(data)"
+        case let .backendFailure(code: code):
+            return "Backend failure \(code)"
+        case .airplaneModeFailure:
+            return "Airplane mode failure"
+        }
+    }
+}
+
+//: Bonus
+
+enum MyOptional<Element> {
+    case some(Element)
+    case none
+}
+
+// In Swift's standard Optional, this type has some syntactic suger
+// let x: Optional<Int> = 4 // same as .some(4)
+// let y: Optional<Int> = nil // same as .none
+
+//: This is just the option in Swift looks like
+
+//: Note: .first exists on sequences in Swift
+func head(arr: [Int]) -> Optional<Int> {
+    if arr.count == 0 {
+        return .none
+    } else {
+        return .some(arr[0])
+    }
+}
+
+// Protip: You can do this today with the PI_ONE_OF macro, but it's native in swift.
+
+/*:
+Excersize?
+ 
+```
+ (.none).getOrElse(4) => 4
+ (.some("a")).getOrElse("") => "a"
+```
+ */
+ 
+// Protip: This is equivalent to Swift Optional's `??` operator
+extension MyOptional {
+    func getOrElse(orElse fallback: Element) -> Element {
+        switch self {
+        case .some(let x):
+            return x
+        case .none:
+            return fallback
+        }
+    }
+}
+
+//: Even linked-lists can be modelled as enums
+
+indirect enum List<T> {
+    case empty
+    case cons(T, List<T>)
+}
+
+
